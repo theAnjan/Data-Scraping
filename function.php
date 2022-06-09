96e6d9409560fbae3a099aa7a59e644e00127311
theAnjan <anjanpaudel.git@gmail.com>
2022-06-09 13:56:38 +0545

    updated

--- function.php
+++ function.php
@@ -1,21 +1,21 @@
 <?php 
 
 function getArticleList($category, $number){
-$file = file_get_contents("https://bg.annapurnapost.com/api/news/list?page=1&per_page={$number}&category_alias={$category}&isCategoryPage=1");
+	$file = file_get_contents("https://bg.annapurnapost.com/api/news/list?page=1&per_page={$number}&category_alias={$category}&isCategoryPage=1");
 
-$enc = json_decode($file, true);
+	$enc = json_decode($file, true);
 
-return $enc['data'];
+	return $enc['data'];
 }
 
 function getOneArticle($id, $slug){
 	$file = file_get_contents("https://annapurnapost.com/news/{$slug}-{$id}");
 
 
-$dom = new domDocument;
-@$dom->loadHTML($file);
+	$dom = new domDocument;
+	@$dom->loadHTML($file);
 
-return $dom;
+	return $dom;
 }
 
 ?>
